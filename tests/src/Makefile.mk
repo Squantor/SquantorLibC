9a66d44bb0ac44068d2d9e4eb4efdeddf162e900
Bart Bilos <squantmuts@gmail.com>
2019-03-02 23:26:01 +0100

    libc enabled, removing junk libc has been enabled for now as I am looking for a good x86_64 startup code. removed defunct test code

--- tests/src/Makefile.mk
+++ tests/src/Makefile.mk
@@ -8,7 +8,7 @@ INCLUDES = -Iinc -I../inc
 COMPILE_C_FLAGS += -std=gnu11 -Wall -Wextra
 COMPILE_CXX_FLAGS += -std=c++17 -Wall -Wextra
 COMPILE_ASM_FLAGS += -c -x assembler-with-cpp
-LINK_FLAGS += -flto -Xlinker --gc-sections -Xlinker -print-memory-usage
+LINK_FLAGS += -flto -Xlinker --gc-sections -Xlinker -print-memory-usage -Xlinker -nolibc
 RLIBDIR = -L"../bin/PC/release"
 RLIBS = -lsqlibc
 DLIBDIR = -L"../bin/PC/debug"
