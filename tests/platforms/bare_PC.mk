a1d1f114273d454f74a6467c5df8772c551c6352
Bart Bilos <squantmuts@gmail.com>
2019-03-06 20:32:46 +0100

    moved rt0 files

--- tests/platforms/bare_PC.mk
+++ tests/platforms/bare_PC.mk
@@ -1,7 +1,8 @@
 # platform settings for PC's with GCC and NO standard libraries
 TOOLCHAIN_PREFIX =
-C_SOURCES += src/00_start.c src/_exit.c
-S_SOURCES += src/startup.s
+C_SOURCES += src/rt0/00_start.c src/rt0/_exit.c
+CXX_SOURCES +=
+S_SOURCES += src/rt0/startup.s
 COMPILE_C_FLAGS += -nostdlib -fomit-frame-pointer -fno-stack-protector -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-unroll-loops -fmerge-all-constants -fno-ident -mfpmath=sse -mfancy-math-387 -ffunction-sections -fdata-sections 
 COMPILE_CXX_FLAGS +=
 COMPILE_ASM_FLAGS += -c -x assembler-with-cpp
