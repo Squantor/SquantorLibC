3b6ff16d4ce8fb3b94a212b08b2e5f5644298e19
Bart Bilos <squantmuts@gmail.com>
2019-03-29 11:39:34 +0100

    small style changes, added strcpy to string.h

--- inc/string.h
+++ inc/string.h
@@ -30,18 +30,19 @@ extern "C" {
 
 #include <stddef.h>
 
-void * memchr(const void * s, int c, size_t n);
-void * memset(void * s, int c, size_t n);
-void * memmove(void * s1, const void * s2, size_t n);
-void * memcpy(void * restrict s1, const void * restrict s2, size_t n);
-int memcmp(const void * s1, const void * s2, size_t n);
+void *memchr(const void *s, int c, size_t n);
+void *memset(void *s, int c, size_t n);
+void *memmove(void *s1, const void * s2, size_t n);
+void *memcpy(void *restrict s1, const void *restrict s2, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
 
-size_t strlen(const char * str);
-size_t strnlen(const char * str, size_t num);
-int strcmp(const char * s1, const char * s2);
+size_t strlen(const char *str);
+size_t strnlen(const char *str, size_t num);
+int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
-char * strncpy(char * restrict s1, const char * restrict s2, size_t n);
-char * strtok_r(char * restrict s1, const char * restrict s2, char ** pos);
+char *strncpy(char *restrict s1, const char *restrict s2, size_t n);
+char *strcpy(char *restrict s1, const char *restrict s2);
+char *strtok_r(char *restrict s1, const char *restrict s2, char ** pos);
 
 #ifdef __cplusplus
 }
