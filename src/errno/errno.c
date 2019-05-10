6049d3189c59204689ed07c20479db6ddc4444a0
Bart Bilos <squantmuts@gmail.com>
2019-05-10 10:39:42 +0200

    added errno functionality

--- src/errno/errno.c
+++ src/errno/errno.c
@@ -21,21 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#ifndef ERRNO_H
-#define ERRNO_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <errno.h>
 
-extern int errno;
-
-#define ERANGE 34
-#define EDOM 33
-#define EILSEQ 84
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+int errno;
